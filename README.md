f22accbecd171cbbc38b2d457580afea9d15edbc
Petteri Leino <petewaterz@gmail.com>
2023-06-21 09:16:17 +0300

    app initialized

---
+++ README.md
@@ -0,0 +1,9 @@
+### Study log
+
+Attempt to branch out technology-wise instead of doing everything from the web dev point of view.
+
+## Technologies:
+
+- Python
+- Tkinter
+- Customtkinter
---
+++ main.py
@@ -0,0 +1,20 @@
+import tkinter
+import customtkinter
+
+customtkinter.set_appearance_mode("System")
+customtkinter.set_default_color_theme("blue")
+
+app = customtkinter.CTk()
+app.geometry("720x480")
+app.title("Study log")
+
+
+def button_function():
+    print("button pressed")
+
+
+# Use CTkButton instead of tkinter Button
+button = customtkinter.CTkButton(master=app, text="CTkButton", command=button_function)
+button.place(relx=0.5, rely=0.5, anchor=customtkinter.CENTER)
+
+app.mainloop()
