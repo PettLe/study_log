48de7d8efe885ff84164d59d904fd098075745f9
Petteri Leino <petewaterz@gmail.com>
2023-06-23 08:10:33 +0300

    made overall tab layout make more sense. Added average grade and total osp

--- main.py
+++ main.py
@@ -28,30 +28,60 @@ kurssit = [kurssiX, kurssiY, kurssiZ]
 # Render the contents of overall tab
 def render_results(kurssit):
     i = 1
+    arvosana = 0
+    arvosanat_lasketut = 0
+    osp = 0
     for course in kurssit:
-        # tab2.grid_columnconfigure((0, 1), weight=1)
+        osp += int(course.osp)
+        try:
+            arvosana += int(course.arvosana)
+            arvosanat_lasketut += 1
+        except:
+            continue
+    if arvosana == 0:
+        arvosana = "-"
+    else:
+        arvosana = arvosana / arvosanat_lasketut
+    # arvosana = arvosana / arvosanat_lasketut
+    # print(arvosana)
 
-        kurssi_label = Label(tab2, text="Kurssin nimi:")
-        kurssi_label.grid(row=i, column=0, sticky="ew", pady=(5, 2))
+    # Render labels for each category
+    kurssi_label = Label(tab2, text="Kurssin nimi:")
+    kurssi_label.grid(row=i, column=0, sticky="ew", pady=(5, 2))
 
-        osp_label = Label(tab2, text="Opintopisteet:")
-        osp_label.grid(row=i + 1, column=0, sticky="ew", pady=2)
+    osp_label = Label(tab2, text="Opintopisteet:")
+    osp_label.grid(row=i, column=1, sticky="ew", pady=2)
 
-        arvosana_label = Label(tab2, text="Arvosana:")
-        arvosana_label.grid(row=i + 2, column=0, sticky="ew", pady=2)
+    arvosana_label = Label(tab2, text="Arvosana:")
+    arvosana_label.grid(row=i, column=2, sticky="ew", pady=2)
 
-        kategoria_label = Label(tab2, text="Kategoria:")
-        kategoria_label.grid(row=i + 3, column=0, sticky="ew", pady=2)
-        # print(course)
+    kategoria_label = Label(tab2, text="Kategoria:")
+    kategoria_label.grid(row=i, column=3, sticky="ew", pady=2)
+
+    for course in kurssit:
         kurssi_data = Label(tab2, text=course.nimi)
-        kurssi_data.grid(row=i, column=1, sticky="w", pady=(5, 2), columnspan=2)
+        kurssi_data.grid(row=i + 1, column=0, sticky="w", pady=(5, 2))
         osp_data = Label(tab2, text=course.osp)
-        osp_data.grid(row=i + 1, column=1, sticky="w", pady=2, columnspan=2)
+        osp_data.grid(row=i + 1, column=1, sticky="ew", pady=2)
         arvosana_data = Label(tab2, text=course.arvosana)
-        arvosana_data.grid(row=i + 2, column=1, sticky="w", pady=2, columnspan=2)
+        arvosana_data.grid(row=i + 1, column=2, sticky="ew", pady=2)
         kategoria_data = Label(tab2, text=course.kategoria)
-        kategoria_data.grid(row=i + 3, column=1, sticky="w", pady=2, columnspan=2)
-        i += 4
+        kategoria_data.grid(row=i + 1, column=3, sticky="w", pady=2)
+
+        i += 1
+    osp_label = Label(tab2, text="Osp yht:", font=("Helvetica", 10, "bold"))
+    osp_label.grid(row=i + 1, column=0, sticky="ew", pady=2)
+
+    osp_data = Label(tab2, text=osp, font=("Helvetica", 10, "bold"))
+    osp_data.grid(row=i + 1, column=1, sticky="ew", pady=2)
+
+    arvosana_label = Label(tab2, text="Keskiarvo:", font=("Helvetica", 10, "bold"))
+    arvosana_label.grid(row=i + 1, column=2, sticky="ew", pady=2)
+
+    if arvosana == 0:
+        arvosana = "-"
+    arvosana_data = Label(tab2, text=(arvosana), font=("Helvetica", 10, "bold"))
+    arvosana_data.grid(row=i + 1, column=3, sticky="ew", pady=2)
 
 
 def testi():
@@ -110,7 +140,7 @@ tab1_2.pack(fill="both", expand=1)
 
 
 tab2 = Frame(tab1_2, width=500, height=1500)
-tab2.grid_columnconfigure((0, 1, 2), weight=1)
+tab2.grid_columnconfigure((0, 1, 2, 3), weight=1)
 tab1_2.create_window((500, 500), window=tab2, anchor="nw")
 tab1_2.config(scrollregion=tab1_2.bbox("all"))
 tab1_2.config(yscrollcommand=ctk_textbox_scrollbar.set)
@@ -132,10 +162,10 @@ clicked2.set("all")
 lajittelu_label = Label(tab2, text="Sort by:")
 lajittelu_label.grid(row=0, column=0, sticky="ew", pady=(5, 2))
 lajittelu_input = OptionMenu(tab2, clicked2, *options2)
-lajittelu_input.grid(row=0, column=1, sticky="w", pady=(5, 2))
+lajittelu_input.grid(row=0, column=1, sticky="w", pady=(5, 2), columnspan=2)
 
 btn2 = Button(tab2, text="Päivitä", command=updateSort)
-btn2.grid(row=0, column=2, sticky="w", pady=(5, 2))
+btn2.grid(row=0, column=3, sticky="w", pady=(5, 2))
 
 # Try this to identify selected tab (and make scrollbar appea only in selected tab)
 # print(tab_view.tab(tab_view.select(), "text"))
